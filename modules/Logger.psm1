2c5cd3ce8b71509ab107f116d2711123cfebde78
brunokupper <119888107+brunokupper@users.noreply.github.com>
2026-01-22 02:53:07 -0300

    Update Logger.psm1

--- modules/Logger.psm1
+++ modules/Logger.psm1
@@ -1,3 +1,6 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+
 # ============================================================
 # NEO VCORE V6 - SISTEMA DE LOGS
 # ============================================================
@@ -121,3 +124,4 @@ function Clear-Logs {
 
     Start-Sleep 1
 }
+
